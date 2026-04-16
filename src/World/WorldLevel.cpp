a5c6305769c701210c514017fa536cb6fb45d19e
le10x <leanderdrb@gmail.com>
2026-04-16 00:52:55 -0400

    Refactor WorldLevelPopup setup and title
    Updated WorldLevelPopup to set a title and adjusted label positioning.

--- src/World/WorldLevel.cpp
+++ src/World/WorldLevel.cpp
@@ -1,22 +1,22 @@
 #include <Geode/Geode.hpp>
-#include "WorldLevel.hpp"
-#include <Geode/Geode.hpp>
 #include <Geode/ui/Popup.hpp>
+#include "WorldLevel.hpp"
 
 using namespace geode::prelude;
 
-
+// Definición de la clase que hereda de Popup
 class WorldLevelPopup : public geode::Popup<std::string const&> {
 protected:
     bool setup(std::string const& value) override {
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
         this->setID("worldLevelPopup"_spr);
-    
-        this->setTitle("");
+        this->setTitle("Nivel"); // Agregamos un título
 
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
         auto label = CCLabelBMFont::create(value.c_str(), "bigFont.fnt");
-        label->setPosition(winSize / 2);
-        this->addChild(label);
+        
+        // En Geode v5 es mejor usar m_mainLayer para añadir cosas al Popup
+        label->setPosition(m_mainLayer->getContentSize() / 2);
+        m_mainLayer->addChild(label);
 
         return true;
     }
@@ -24,9 +24,8 @@ protected:
 public:
     static WorldLevelPopup* create(std::string const& text) {
         auto ret = new WorldLevelPopup();
-
-        
-        if (ret && ret->initAnchored(300.f, 260.f, text)) {
+        // 400.f es el ancho, 280.f es el alto
+        if (ret && ret->initAnchored(400.f, 280.f, text)) {
             ret->autorelease();
             return ret;
         }
