96b9637e889412808c86a6098e5a09b3281e0e59
le10x <leanderdrb@gmail.com>
2026-04-16 18:23:01 -0400

    Refactor MyCustomLevelPopup setup and create methods

--- src/World/WorldLevel.cpp
+++ src/World/WorldLevel.cpp
@@ -2,28 +2,21 @@
 
 using namespace geode::prelude;
 
-// Implementamos la función setup (que es donde va el contenido del Popup)
-bool MyCustomLevelPopup::setup(std::string const& value) {
+bool MyCustomLevelPopup::setup() {
     this->setTitle("Información");
 
-    auto winSize = CCDirector::sharedDirector()->getWinSize();
-    
-    // Creamos el texto
-    auto label = CCLabelBMFont::create(value.c_str(), "bigFont.fnt");
+    auto label = CCLabelBMFont::create(m_text.c_str(), "bigFont.fnt");
     label->setScale(0.6f);
-    
-    // Lo añadimos al m_mainLayer (la capa principal del Popup en Geode v5)
     label->setPosition(m_mainLayer->getContentSize() / 2);
     m_mainLayer->addChild(label);
 
     return true;
 }
 
-// Implementamos la función create
 MyCustomLevelPopup* MyCustomLevelPopup::create(std::string const& text) {
     auto ret = new MyCustomLevelPopup();
-    // initAnchored crea el cuadro con el ancho y alto especificados
-    if (ret && ret->initAnchored(350.f, 250.f, text)) {
+    ret->m_text = text; // Pasamos el texto manualmente
+    if (ret && ret->initAnchored(350.f, 250.f)) {
         ret->autorelease();
         return ret;
     }
